6ae7f97d336651ca37237d3b14675f9c1196179f
Upasara <mihirangaupasara2000@gmail.com>
2024-11-25 13:20:54 +0530

    create proj files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+			Asap Condensed comes with 8 styles, including semibold, bold and all their
+			respective italics. It’s a handy Google font due to its standardized
+			character width, making it easy to change styles without having to adjust
+			the text. It’s a good condensed typeface that can make the most of headers
+			and titles.
+		</div>
+	);
+};
+
+export default Home;
